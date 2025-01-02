bf9ef685079af0506966f4e76050c9257f2c4706
KaDzikowicz <164359113+KaDzikowicz@users.noreply.github.com>
2025-01-02 13:26:51 +0100

    Update storage_types.hxx

--- include/storage_types.hxx
+++ include/storage_types.hxx
@@ -4,5 +4,21 @@ enum class PackageQueueType {
 };
 
 class IPackageStockpile {
+    public:
+        void push(Package&&){}
 
+        bool empty() {query}
+
+        std::size_t size() {query}
+
+        //iteratory - do zrobienia
+
+        ~IPackageStockpile()
+};
+
+class IPackageQueue{
+    public:
+        Package pop() {}
+
+        PackageQueueType get_queue_type() {query}
 };
