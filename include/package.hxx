a757ebe5502f77289461bff77296158e46f09b4d
MarcinDuplaga <129391153+MarcinDuplaga@users.noreply.github.com>
2025-01-02 13:21:10 +0100

    Plik nagłówkowy dla biblioteki z klasą Package

--- include/package.hxx
+++ include/package.hxx
@@ -1,5 +1,21 @@
+#ifndef PACKAGE_HPP
+#define PACKAGE_HPP
 #include "types.hxx"
 
 class Package {
+  public:
+    //konstruktory
+    Package() : id(0) {}
+    Package(ElementID id) : id(id) {};
+    Package(Package&& other) noexcept;
+    //metody
+    Package& operator=(Package&& other) noexcept;
+    ElementID get_id() const;
+    //destruktor
+    ~Package() {};
+
+  private:
+      ElementID id;
     
 };
+#endif //PACKAGE_HPP
