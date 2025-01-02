2e00d3fbc92ce334e87213bd8a854e935bd607fa
MarcinDuplaga <129391153+MarcinDuplaga@users.noreply.github.com>
2025-01-02 13:20:26 +0100

    Dodanie funkcjonalności klasy package, zgodnie z diagramem

--- src/package.cpp
+++ src/package.cpp
@@ -1,1 +1,22 @@
 #include "package.hxx"
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+
+Package::Package(){}
+Package::Package(ElementID id) : id(id){}
+Package::Package(Package&& other) : id(std::move(other.id)){
+  other.id = 0;
+}
+
+Package& Package::operator=(Package& other){
+  if (this != &other){
+    id = std::move(other.id);
+    other.id = 0;
+  }
+  return *this;
+}
+
+ElementID Package::getID() const{
+  return id;
+}
