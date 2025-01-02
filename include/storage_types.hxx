99abc4e01d01e69b22fd7c18f51fb7786dc52b5c
MarcinDuplaga <129391153+MarcinDuplaga@users.noreply.github.com>
2025-01-02 13:44:47 +0100

    header guard poprawa seantyki

--- include/storage_types.hxx
+++ include/storage_types.hxx
@@ -1,29 +1,22 @@
+#ifndef STORAGE_TYPES_HPP
+#define STORAGE_TYPES_HPP
+#include "package.hxx"
+#include <cstdlib>
 enum class PackageQueueType {
     FIFO,
     LIFO
 };
 
 class IPackageStockpile {
-    public:
-        void push(Package&&){}
-
-        bool empty() {query}
-
-        std::size_t size() {query}
-
-        //iteratory - do zrobienia
-
-        ~IPackageStockpile()
+ public:
+     void push(Package&&);
+     bool empty() const {query}
+     std::size_t size() const {query}
+     ~IPackageStockpile() {}
 };
 
 class IPackageQueue{
-    public:
-        Package pop() {}
-
-        PackageQueueType get_queue_type() {query}
-};
-
-class PackageQueue(IPackageQueue){
-        public:
-        PackageQueue(PackageQueueType):{}
-};
+  public:
+    Package pop();
+    PackageQueueType get_queue_type() const {query};
+#endif //STORAGE_TYPES_HPP
