dec46b0044b2b9d89aebf44f2e0c7f0245a1f34c
Karol Dzielski <karol.dzielski2004@gmail.com>
2025-01-03 15:10:44 +0100

    Dodanie reszty plikow

---
+++ include/nodes.hxx
@@ -0,0 +1,8 @@
+//
+// Created by User on 03.01.2025.
+//
+
+#ifndef NODES_HXX
+#define NODES_HXX
+
+#endif //NODES_HXX
---
+++ include/vertex_types.hxx
@@ -0,0 +1,16 @@
+//
+// Created by User on 03.01.2025.
+//
+
+#ifndef VERTEX_TYPES_HXX
+#define VERTEX_TYPES_HXX
+
+
+enum class PackageQueueType
+{
+    RAMP,
+    WORKER,
+    WAREHOUSE
+};
+
+#endif //VERTEX_TYPES_HXX
