f2afc16db7a1469dfa263f6f1781aafc0ea5c48b
KDzikowicz <kdzikowicz@student.agh.edu.pl>
2025-01-03 15:47:23 +0100

    Rozpisanie diagramu w nodes.hxx //cz.1

--- include/nodes.hxx
+++ include/nodes.hxx
@@ -5,4 +5,41 @@
 #ifndef NODES_HXX
 #define NODES_HXX
 
+#include <cstdlib>
+#include <optional>
+#include "types.hxx"
+#include "package.hxx"
+#include "storage_types.hxx"
+
+class Ramp{
+    public:
+        Ramp(ElementID id,TimeOffset di);
+        void deliver_goods(Time t);
+        TimeOffset get_delivery_interval() const;
+        ElementID det_id(void) const;
+};
+
+class ReceiverPreferences{
+    public:
+        using preferences_t = std::map<IPackageReceiver*, double>;
+        using const_iterator = preferences_t::const_iterator;
+
+        ReceiverPreferences(ProbabilityGenerator pg);
+        void add_receiver(IPackageReceiver* r);
+        void add_receiver(IPackageReceiver* r);
+        void remove_receiver(IPackageReceiver* r);
+        IPackageReceiver* choose_receiver(void);
+        preferences_t& get_preferences(void) const;
+};
+
+class PackageSender: public IPackageReceiver{
+    public:
+        PackageSender(PackageSender&&);
+        ReceiverPreferences receiver_preferences_();
+        void send_package(void);
+        std::optional<Package>& get_sending_buffer(void) const;
+    protected:
+        void push_package(Package&&);
+};
+
 #endif //NODES_HXX
