456eedf70e34dd55860998f33be06f167341fc99
KDzikowicz <kdzikowicz@student.agh.edu.pl>
2025-01-03 13:14:17 +0100

    Dokończono PackageQueue::pop()

--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -1,32 +1,29 @@
 #include "storage_types.hxx"
 
 Package PackageQueue::pop() {
-    switch(type_) {
-        case PackageQueueType::FIFO: {
-            Package& p = queue_.front();
-            queue_.pop_front();
-            return p;
-        }
-        case PackageQueueType::LIFO: {
-            Package& p = queue_.back();
-            queue_.pop_back();
-            return p;
-        }
-        default:
-            break;
-    };
+    Package p;
+    if(type_ == PackageQueueType::FIFO){
+        p = std::move(queue_.front());
+        queue_.pop_front();
+        return p;
+    }
+    else{
+        p = std::move(queue_.back());
+        queue_.pop_back();
+        return p;
+    }
 }
 
 void PackageQueue::push(Package&&){
  };
 
-bool PackageQueue::empty(){
+bool PackageQueue::empty() const{
     if(PackageQueue::queue_.empty())
         return True;
     else 
         return False;
 };
 
-std::size_t PackageQueue::size(){
+std::size_t PackageQueue::size() const{
     return PackageQueue::queue_.length();
 };
