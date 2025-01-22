8bf86e4056676acca90ea17bb3f2720275587781
MarcinDuplaga <129391153+MarcinDuplaga@users.noreply.github.com>
2025-01-22 10:15:16 +0100

    Factory.cpp dodano do_xxx() metody

--- src/factory.cpp
+++ src/factory.cpp
@@ -1,7 +1,24 @@
 #include "factory.hxx"
 #include "nodes.hxx"
 
-bool Factory::is_consistent() const;
-void Factory::do_deliveries(Time t);
-void Factory::do_package_passing(void);
-void Factory::do_work(Time t);
+bool Factory::is_consistent() const{
+  return 0
+}
+
+void Factory::do_deliveries(Time t){
+  for (auto &ramp : cont_r)
+        ramp.deliver_goods(t);
+}
+
+void Factory::do_package_passing(void){
+  for (auto &ramp : cont_r)
+        ramp.send_package();
+
+    for (auto &worker : cont_w)
+        worker.send_package();
+}
+
+oid Factory::do_work(Time t){
+  for(auto &ramp : const_w)
+    worker.do_work(t)
+}
