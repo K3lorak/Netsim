0ff1eae7ee97368d6cc74999af388fd01599cdc1
MarcinDuplaga <129391153+MarcinDuplaga@users.noreply.github.com>
2025-01-22 11:12:25 +0100

    load save i parse

--- src/factory.cpp
+++ src/factory.cpp
@@ -49,4 +49,114 @@ void Factory::remove_storehouse(ElementID id)
     });
     cont_s.remove_by_id(id);
 }
+ParsedLineData parse_line(std::string& line){
+    
+  std::vector<std::string> tokens;
+  std::string token;
+    
+  std::istringstream token_stream(line);
+  char delimiter = ' ';
 
+  while (std::getline(token_stream, token, delimiter))
+        tokens.push_back(token);
+
+  ParsedLineData parsed_data;
+
+  std::map<std::string, ElementType> element_types{
+            {"RAMP", ElementType::RAMP},
+            {"WORKER", ElementType::WORKER},
+            {"STOREHOUSE", ElementType::STOREHOUSE},
+            {"LINK", ElementType::LINK},
+    };
+
+    try {
+        parsed_data.element_type = element_types.at(tokens[0]);
+
+        std::for_each(std::next(tokens.cbegin()), tokens.cend(), [&](const std::string& parameter_str) {
+            auto key_value = character_split(parameter_str, '=');
+            parsed_data.parameters[key_value[0]] = key_value[1];
+        });
+    } catch (std::out_of_range& ex) {
+        throw std::exception();
+    }
+
+    return parsed_data;
+}
+};
+
+Factory load_factory_structure(std::istream& is){
+    Factory factory;
+
+    std::string line;
+
+    while (std::getline(is, line)) {
+        if (line.empty() || line[0] == ';')
+            continue;
+
+        ParsedLineData parsed = parse_line(line);
+
+        switch(parsed.element_type) {
+            case ElementType::RAMP: {
+                ElementID element_id = std::stoi(parsed.parameters.at("id"));
+                TimeOffset delivery_interval = std::stoi(parsed.parameters.at("delivery-interval"));
+                Ramp ramp(element_id, delivery_interval);
+
+                factory.add_ramp(std::move(ramp));
+                break;
+            }
+            case ElementType::WORKER: {
+                ElementID element_id = std::stoi(parsed.parameters.at("id"));
+                TimeOffset processing_time = std::stoi(parsed.parameters.at("processing-time"));
+                PackageQueueType package_queue_t = get_package_queue_type(parsed.parameters.at("queue-type"));
+                Worker worker(element_id, processing_time, std::make_unique<PackageQueue>(package_queue_t));
+
+                factory.add_worker(std::move(worker));
+                break;
+            }
+            case ElementType::STOREHOUSE: {
+                ElementID element_id = std::stoi(parsed.parameters.at("id"));
+                Storehouse storehouse(element_id);
+
+                factory.add_storehouse(std::move(storehouse));
+                break;
+            }
+            case ElementType::LINK: {
+                link(factory, parsed.parameters);
+                break;
+            }
+        }
+    }
+
+    return factory;
+}
+void save_factory_structure(Factory& factory, std::ostream& os) {
+  
+
+    std::stringstream link_stream;
+
+    std::for_each(factory.ramp_cbegin(), factory.ramp_cend(), [&](const Ramp& ramp) {
+        ElementID ramp_id = ramp.get_id();
+        os << "RAMP id=" << ramp_id << ' '
+           << "delivery-interval=" << ramp.get_delivery_interval() << '\n';
+
+        link_stream_fill(link_stream, ramp, ramp_id, "ramp");
+    });
+
+    std::for_each(factory.worker_cbegin(), factory.worker_cend(), [&](const Worker& worker) {
+        PackageQueueType queue_type = worker.get_queue()->get_queue_type();
+        ElementID worker_id = worker.get_id();
+        os << "WORKER id=" << worker_id << ' '
+           << "processing-time=" << worker.get_processing_duration() << ' '
+           << "queue-type=" << queue_type_str(queue_type) << '\n';
+
+        link_stream_fill(link_stream, worker, worker_id, "worker");
+    });
+
+    std::for_each(factory.storehouse_cbegin(), factory.storehouse_cend(), [&](const Storehouse& storehouse) {
+        os << "STOREHOUSE id=" << storehouse.get_id() << '\n';
+    });
+
+    os << link_stream.str();
+
+    os.flush();
+}
