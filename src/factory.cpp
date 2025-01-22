f98be9c292946f80b181e4e4a770c6576c044633
MarcinDuplaga <129391153+MarcinDuplaga@users.noreply.github.com>
2025-01-22 10:08:46 +0100

    Update factory.cpp

--- src/factory.cpp
+++ src/factory.cpp
@@ -1,1 +1,7 @@
+#include "factory.hxx"
+#include "nodes.hxx"
 
+bool Factory::is_consistent() const;
+void Factory::do_deliveries(Time t);
+void Factory::do_package_passing(void);
+void Factory::do_work(Time t);
