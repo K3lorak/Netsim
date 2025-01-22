ed62d3d1b997211c90af88268100a5642ef834b5
MarcinDuplaga <129391153+MarcinDuplaga@users.noreply.github.com>
2025-01-22 10:38:01 +0100

    Remove storehouse and worker

--- src/factory.cpp
+++ src/factory.cpp
@@ -18,7 +18,35 @@ void Factory::do_package_passing(void){
         worker.send_package();
 }
 
-oid Factory::do_work(Time t){
+void Factory::do_work(Time t){
   for(auto &ramp : const_w)
     worker.do_work(t)
 }
+
+void Factory::remove_worker(ElementID id){
+  Worker* node = &(*cont_w.find_by_id(id));
+
+  std::for_each(cont_w.begin(), cont_w.end(), [node](Worker& worker) {
+        worker.receiver_preferences_.remove_receiver(node);
+    });
+
+  std::for_each(cont_r.begin(), cont_r.end(), [node](Ramp& ramp) {
+        ramp.receiver_preferences_.remove_receiver(node);
+    });
+
+  cont_w.remove_by_id(id);
+}
+
+void Factory::remove_storehouse(ElementID id)
+{
+    Storehouse* node = &(*cont_s.find_by_id(id));
+    std::for_each(cont_w.begin(), cont_w.end(), [&node](Worker& ramp) {
+        ramp.receiver_preferences_.remove_receiver(node);
+    });
+
+    std::for_each(cont_w.begin(), cont_w.end(), [&node](Worker& worker) {
+        worker.receiver_preferences_.remove_receiver(node);
+    });
+    cont_s.remove_by_id(id);
+}
+
